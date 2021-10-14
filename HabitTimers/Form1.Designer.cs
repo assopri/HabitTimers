8228e78181ad79524f6806c299e4ee9fa6de2fc2
assopri <i4G4fyaa>
2021-10-14 21:44:13 +0300

    nsdr and basic pomodoro

--- HabitTimers/Form1.Designer.cs
+++ HabitTimers/Form1.Designer.cs
@@ -28,34 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tbLaunchPeriodicTimer = new System.Windows.Forms.Button();
+            this.tbLaunchNSDR = new System.Windows.Forms.Button();
+            this.btPomodoro = new System.Windows.Forms.Button();
+            this.sePomodoroPeriod = new DevExpress.XtraEditors.SpinEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.sePomodoroPeriod.Properties)).BeginInit();
             this.SuspendLayout();
             // 
-            // tbLaunchPeriodicTimer
+            // tbLaunchNSDR
             // 
-            this.tbLaunchPeriodicTimer.Location = new System.Drawing.Point(249, 12);
-            this.tbLaunchPeriodicTimer.Name = "tbLaunchPeriodicTimer";
-            this.tbLaunchPeriodicTimer.Size = new System.Drawing.Size(163, 23);
-            this.tbLaunchPeriodicTimer.TabIndex = 0;
-            this.tbLaunchPeriodicTimer.Text = "Launch Periodic Timer";
-            this.tbLaunchPeriodicTimer.UseVisualStyleBackColor = true;
-            this.tbLaunchPeriodicTimer.Click += new System.EventHandler(this.tbLaunchPeriodicTimer_Click);
+            this.tbLaunchNSDR.Location = new System.Drawing.Point(249, 12);
+            this.tbLaunchNSDR.Name = "tbLaunchNSDR";
+            this.tbLaunchNSDR.Size = new System.Drawing.Size(163, 23);
+            this.tbLaunchNSDR.TabIndex = 0;
+            this.tbLaunchNSDR.Text = "NSDR";
+            this.tbLaunchNSDR.UseVisualStyleBackColor = true;
+            this.tbLaunchNSDR.Click += new System.EventHandler(this.tbLaunchPeriodicTimer_Click);
+            // 
+            // btPomodoro
+            // 
+            this.btPomodoro.Location = new System.Drawing.Point(280, 79);
+            this.btPomodoro.Name = "btPomodoro";
+            this.btPomodoro.Size = new System.Drawing.Size(173, 23);
+            this.btPomodoro.TabIndex = 1;
+            this.btPomodoro.Text = "Pomodoro Timer";
+            this.btPomodoro.UseVisualStyleBackColor = true;
+            this.btPomodoro.Click += new System.EventHandler(this.btPomodoro_Click);
+            // 
+            // sePomodoroPeriod
+            // 
+            this.sePomodoroPeriod.EditValue = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.sePomodoroPeriod.Location = new System.Drawing.Point(503, 81);
+            this.sePomodoroPeriod.Name = "sePomodoroPeriod";
+            this.sePomodoroPeriod.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.sePomodoroPeriod.Properties.Increment = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.sePomodoroPeriod.Properties.IsFloatValue = false;
+            this.sePomodoroPeriod.Properties.MaskSettings.Set("mask", "N00");
+            this.sePomodoroPeriod.Properties.MaxValue = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.sePomodoroPeriod.Size = new System.Drawing.Size(100, 20);
+            this.sePomodoroPeriod.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(648, 277);
-            this.Controls.Add(this.tbLaunchPeriodicTimer);
+            this.Controls.Add(this.sePomodoroPeriod);
+            this.Controls.Add(this.btPomodoro);
+            this.Controls.Add(this.tbLaunchNSDR);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.sePomodoroPeriod.Properties)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button tbLaunchPeriodicTimer;
+        private System.Windows.Forms.Button tbLaunchNSDR;
+        private System.Windows.Forms.Button btPomodoro;
+        private DevExpress.XtraEditors.SpinEdit sePomodoroPeriod;
     }
 }
 
--- HabitTimers/Form1.cs
+++ HabitTimers/Form1.cs
@@ -3,10 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Speech.Synthesis;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,15 +17,36 @@ namespace HabitTimers
 {
     public partial class Form1 : Form
     {
+        SpeechSynthesizer _sythesizer = new SpeechSynthesizer();
+        bool _pomodoroTimerLaunchedFlag = false;
+        //DateTime _pomodoroTimerStartTime;
+        DateTime _pomodoroTimerFinishTime;
+        DateTime _pomodoroTimerNextStartTime;
         public Form1()
         {
             InitializeComponent();
-            LaunchPeriodicTimer(3);
+
+            LoadFormSettings();
+            // LaunchPeriodicTimer(120);
+        }
+
+        private void LoadFormSettings()
+        {
+            sePomodoroPeriod.Value = Properties.Settings.Default.PomodoroPeriod;
         }
 
         private void tbLaunchPeriodicTimer_Click(object sender, EventArgs e)
         {
-            LaunchPeriodicTimer(3);
+            int prepareSeconds = 5;
+            _sythesizer.Speak("You have " + prepareSeconds + " seconds to prepare for NSDR. Warm your hands. Lay down. After please make see listen feel practice.");
+                Delayed(prepareSeconds, () =>
+                {
+                    _sythesizer.Speak("NSDR Launched");
+                    System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=A6P_xLLlcGQ");
+                    LaunchPeriodicTimer(120);
+                }
+            );
+            
         }
 
         private void LaunchPeriodicTimer(int intervalSec)
@@ -49,7 +72,65 @@ namespace HabitTimers
             //TODO: close started aspp
         }
 
+        private void btPomodoro_Click(object sender, EventArgs e)
+        {
+            if(_pomodoroTimerLaunchedFlag)
+            {
+                _sythesizer.Speak("Timer is now launched. Will finish in " + (_pomodoroTimerFinishTime - DateTime.Now).Seconds + " seconds");
+                return;
+            }
+            if(DateTime.Now < _pomodoroTimerNextStartTime)
+            {
+                _sythesizer.Speak("You should rest " 
+                    + (_pomodoroTimerNextStartTime - DateTime.Now).Seconds + " more seconds");
+                return;
+            }
+            int timeSec = Convert.ToInt32(sePomodoroPeriod.Value);
+            int buffer = timeSec / 4;
 
-        //private void LaunchPeriodicTimer
+
+
+            Delayed(timeSec, () =>
+                { 
+                    _sythesizer.Speak("Main time finished. Left " + buffer + " seconds.");
+                    Delayed(buffer, () =>
+                    {
+                        _sythesizer.Speak("Ready");
+                        _pomodoroTimerLaunchedFlag = false;
+                        _pomodoroTimerNextStartTime = DateTime.Now.AddSeconds(timeSec / 2);
+                        Process.Start("https://www.youtube.com/watch?v=XQuR1OxYJt0");
+                    });
+                }
+            );
+            _sythesizer.Speak("Timer for " + timeSec/60 + " minutes launched.");
+            // _pomodoroTimerStartTime = DateTime.Now;
+            _pomodoroTimerFinishTime = DateTime.Now.AddSeconds(timeSec+ buffer);
+            _pomodoroTimerLaunchedFlag = true;
+            //await Task.Delay(timeSec);
+
+            //sythesizer.SpeakAsync(new Prompt("Main time finished. Left " + buffer + " seconds"));
+
+            //await Task.Delay(buffer);
+
+            //sythesizer.SpeakAsync(new Prompt("Ready")); 
+
+        }
+
+        public void Delayed(int delaySec, Action action)
+        {
+            Timer timer = new Timer();
+            timer.Interval = delaySec * 1000;
+            timer.Tick += (s, e) => {
+                action();
+                timer.Stop();
+            };
+            timer.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.PomodoroPeriod = (int)sePomodoroPeriod.Value;
+            Properties.Settings.Default.Save();
+        }
     }
 }
